bcec5ebc7cb6f4bbce725c58b50176bce5b251bf
joaquinharguindeguy <jaharguindeguy@mail.austral.edu.ar>
2025-04-04 09:35:45 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,7 +1,7 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
-    print(f'The result of {x} comes first than {y} is {x>y}')
-    print(f'The result of {y} comes first than {x} is {y>x}')
+    print(f'The result of {x} comes first than {y} is {x<y}')
+    print(f'The result of {y} comes first than {x} is {y<x}')
 
 earth()
